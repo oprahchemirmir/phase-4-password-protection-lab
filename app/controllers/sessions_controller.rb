da951830cebcc2833e41759dcddc43dc92c68a01
oprah <oprah.chemirmir@student.moringaschool.com>
2023-03-26 17:20:18 +0300

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+
+
+class SessionsController < ApplicationController
+    def create
+      user = User.find_by(username: params[:username])
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :ok
+      else
+        render json: { error: "Invalid username or password" }, status: :unauthorized
+      end
+    end
+  
+    def destroy
+      session.delete :user_id
+      render json: {}, status: :no_content
+    end
+  end
